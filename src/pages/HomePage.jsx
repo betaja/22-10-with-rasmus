b30e08b9e0333ea029d0485ae9a2d639aa21f057
betaja <144784304+betaja@users.noreply.github.com>
2024-10-22 13:40:55 +0200

    22.10

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
+
 export default function HomePage() {
-    return (
-        <section className="page">
-            <h1>Home Page</h1>
-            <p>Home is where the heart is 💛</p>
-            <p>Oh My, sounds like a bad movie!</p>
-        </section>
-    );
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetchPosts();
+    async function fetchPosts() {
+      const url = "https://with-rasmus-default-rtdb.firebaseio.com/posts.json";
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      const postsArray = Object.keys(data).map((postId) => ({
+        id: postId,
+        ...data[postId],
+      }));
+      console.log(postsArray);
+      setPosts(postsArray);
+    }
+  }, []);
+
+  return (
+    <section className="page">
+      <div className="grid">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+    </section>
+  );
 }
